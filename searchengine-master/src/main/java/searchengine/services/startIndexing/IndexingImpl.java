7b37af39fd845fdc333afbc9299c5af56b11ef32
Programmer <Ozsfag3154Artem@gmail.com>
2023-10-13 15:54:09 +0900

    all works #2

--- searchengine-master/src/main/java/searchengine/services/startIndexing/IndexingImpl.java
+++ searchengine-master/src/main/java/searchengine/services/startIndexing/IndexingImpl.java
@@ -23,6 +23,7 @@ import java.util.concurrent.RecursiveAction;
 @Service
 @RequiredArgsConstructor
 public class IndexingImpl implements IndexingService {
+
     @Autowired
     SitesList sitesList;
     @Autowired
